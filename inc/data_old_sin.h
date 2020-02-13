3e64375142d68e19db21b0885e9ca6bc3fe9ad22
M Jayadharan <manu.jayadharan@gmail.com>
2020-02-12 23:49:38 -0500

    changed pressure to sin(4)*sin(3t)*sin(4t)

--- inc/data_old_sin.h
+++ inc/data_old_sin.h
@@ -103,8 +103,7 @@ namespace vt_darcy
       {
         case 2:
 //          return c0*t_scale*exp(t*t_scale)*(cos(y*3.141592653589793)*sin(x*3.141592653589793)+1.0E1)-exp(t*t_scale)*(y*-2.0+pow(x-1.0,4.0)*pow(y-1.0,2.0)*3.0+x*sin(x*y)*sin(x)+2.0)+(3.141592653589793*3.141592653589793)*exp(t*t_scale)*cos(y*3.141592653589793)*sin(x*3.141592653589793)*2.0;
-//        	return c0*t_scale*exp(t*t_scale)*sin(p[0])*sin(2.0*p[1]) + exp(t*t_scale)*5.0*sin(p[0])*sin(2.0*p[1]);
-        	return 4*cos(4*t)*sin(3*x)*sin(4*y)+sin(4*t)*25*sin(3*x)*sin(4*y);
+        	return c0*t_scale*exp(t*t_scale)*sin(p[0])*sin(2.0*p[1]) + exp(t*t_scale)*5.0*sin(p[0])*sin(2.0*p[1]);
         default:
         Assert(false, ExcMessage("The RHS data for dim != 2 is not provided"));
       }
@@ -137,10 +136,9 @@ namespace vt_darcy
       {
         case 2:
 //          return exp(t*t_scale)*(cos(y*3.141592653589793)*sin(x*3.141592653589793)+1.0E1);
-//        	return exp(t*t_scale)*sin(x)*sin(2.0*y);
-        	return sin(4*t)*sin(3*x)*sin(4*y);
+        	return exp(t*t_scale)*sin(x)*sin(2.0*y);
         case 3:
-        	return sin(4*z)*sin(3*x)*sin(4.0*y);
+        	return exp(z)*sin(x)*sin(2.0*y);
         default:
         Assert(false, ExcMessage("The BC data for dim != 2 is not provided"));
       }
@@ -180,9 +178,9 @@ namespace vt_darcy
 //                values(1) = 3.141592653589793*exp(t*t_scale)*sin(x*3.141592653589793)*sin(y*3.141592653589793);
 //                values(2) = exp(t*t_scale)*(cos(y*3.141592653589793)*sin(x*3.141592653589793)+1.0E1);
 
-            	  values(0) = -sin(4*t)*3*cos(3*x)*sin(4*y) ;
-            	  values(1) = -sin(4*t)*4*sin(3*x)*cos(4*y) ;
-            	  values(2) = sin(4*t)*sin(3*x)*sin(4*y);
+            	  values(0) = -exp(t*t_scale)*cos(p[0])*sin(2.0*p[1]) ;
+            	  values(1) = -exp(t*t_scale)*2.0*sin(p[0])*cos(2.0*p[1]) ;
+            	  values(2) = exp(t*t_scale)*sin(p[0])*sin(2.0*p[1]);
 
 
                 break;
@@ -240,11 +238,11 @@ namespace vt_darcy
 //            grads[1][0] = (3.141592653589793*3.141592653589793)*exp(t*t_scale)*cos(x*3.141592653589793)*sin(y*3.141592653589793);
 //            grads[1][1] = (3.141592653589793*3.141592653589793)*exp(t*t_scale)*cos(y*3.141592653589793)*sin(x*3.141592653589793);
 
-        	 grads[0][0] = sin(4*t)*9*sin(3*x)*sin(4*y);
-        	 grads[0][1] = -sin(4*t)*12.0*cos(3*x)*cos(4*y);
+        	 grads[0][0] = exp(t*t_scale)*sin(p[0])*sin(2.0*p[1]);;
+        	 grads[0][1] = -exp(t*t_scale)*2.0*cos(p[0])*cos(2*p[1]);
 
-        	 grads[1][0] = -sin(4*t)*12.0*cos(3*x)*cos(4*y);
-        	 grads[1][1] = sin(4*t)*16*sin(3*x)*sin(4*y);
+        	 grads[1][0] = -exp(t*t_scale)*2.0*cos(p[0])*cos(2.0*p[1]);
+        	 grads[1][1] = exp(t*t_scale)*4.0*sin(p[0])*sin(2.0*p[1]);
 
             break;
         case 3:
@@ -290,7 +288,7 @@ namespace vt_darcy
               values(1) = 0;
 //              values(2) = (cos(y*M_PI)*sin(x*M_PI)+1.0E1);
 
-              values(2) = sin(4*t)*sin(3*x)*sin(4*y);
+              values(2) = exp(t*t_scale)*sin(p[0])*sin(2.0*p[1]);
           break;
           case 3:
 
