a9af3ca3f13ed7f5eb39b13cf84c6556d54eb216
M Jayadharan <manu.jayadharan@gmail.com>
2020-02-26 23:42:43 -0500

    updated the dta.h to make p(x,y)= sin(8t)*(sin11xCos11y+cos11xSin11y)

--- inc/data.h
+++ inc/data.h
@@ -105,7 +105,9 @@ namespace vt_darcy
 //          return c0*t_scale*exp(t*t_scale)*(cos(y*3.141592653589793)*sin(x*3.141592653589793)+1.0E1)-exp(t*t_scale)*(y*-2.0+pow(x-1.0,4.0)*pow(y-1.0,2.0)*3.0+x*sin(x*y)*sin(x)+2.0)+(3.141592653589793*3.141592653589793)*exp(t*t_scale)*cos(y*3.141592653589793)*sin(x*3.141592653589793)*2.0;
 //        	return c0*t_scale*exp(t*t_scale)*sin(p[0])*sin(2.0*p[1]) + exp(t*t_scale)*5.0*sin(p[0])*sin(2.0*p[1]);
 //        	return 8*cos(8*t)*sin(3*x)*sin(4*y)+sin(8*t)*25*sin(3*x)*sin(4*y);
-        	return 8*cos(8*t)*sin(11*x)*cos(11*y)+sin(8*t)*242*sin(11*x)*cos(11*y);
+//        	return 8*cos(8*t)*sin(11*x)*cos(11*y)+sin(8*t)*242*sin(11*x)*cos(11*y);
+        	return 8*cos(8*t)*(sin(11*x)*cos(11*y)+sin(11*y)*cos(11*x))
+        			+sin(8*t)*242*(sin(11*x)*cos(11*y)+sin(11*y)*cos(11*x));
         default:
         Assert(false, ExcMessage("The RHS data for dim != 2 is not provided"));
       }
@@ -140,10 +142,11 @@ namespace vt_darcy
 //          return exp(t*t_scale)*(cos(y*3.141592653589793)*sin(x*3.141592653589793)+1.0E1);
 //        	return exp(t*t_scale)*sin(x)*sin(2.0*y);
 //        	return sin(8*t)*sin(3*x)*sin(4*y);
-        	return sin(8*t)*sin(11*x)*cos(11*y);
+//        	return sin(8*t)*sin(11*x)*cos(11*y);
+        	return sin(8*t)*( sin(11*x)*cos(11*y) + sin(11*y)*cos(11*x) );
         case 3:
 //        	return sin(8*z)*sin(3*x)*sin(4.0*y);
-        	return sin(8*z)*sin(11*x)*cos(11*y);
+        	return sin(8*z) * ( sin(11*x)*cos(11*y)+sin(11*y)*cos(11*x) );
         default:
         Assert(false, ExcMessage("The BC data for dim != 2 is not provided"));
       }
@@ -186,9 +189,9 @@ namespace vt_darcy
 //            	  values(0) = -sin(8*t)*3*cos(3*x)*sin(4*y) ;
 //            	  values(1) = -sin(8*t)*4*sin(3*x)*cos(4*y) ;
 //            	  values(2) = sin(8*t)*sin(3*x)*sin(4*y);
-          	  values(0) = -sin(8*t)*11*cos(11*x)*cos(11*y) ;
-          	  values(1) = sin(8*t)*11*sin(11*x)*sin(11*y) ;
-          	  values(2) = sin(8*t)*sin(11*x)*cos(11*y);
+          	  values(0) = sin(8*t)*11*( -cos(11*x)*cos(11*y) + sin(11*x)*sin(11*y) ) ;
+          	  values(1) = sin(8*t)*11*( sin(11*x)*sin(11*y) - cos(11*x)*cos(11*y) );
+          	  values(2) = sin(8*t)*( sin(11*x)*cos(11*y) + sin(11*y)*cos(11*x));
 
 
                 break;
@@ -303,7 +306,7 @@ namespace vt_darcy
 //              values(2) = (cos(y*M_PI)*sin(x*M_PI)+1.0E1);
 
 //              values(2) = sin(8*t)*sin(3*x)*sin(4*y);
-              values(2) = sin(8*t)*sin(11*x)*cos(11*y);
+              values(2) = sin(8*t)*( sin(11*x)*cos(11*y) + sin(11*y)*cos(11*x));
           break;
           case 3:
 
